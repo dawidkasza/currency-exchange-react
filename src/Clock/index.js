c855f72c813eb4dfe928afce1e165a44b7ee4e18
Dawid Kasza <dawidkasza@op.pl>
2024-04-05 09:13:49 +0200

    add custom hook

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
 import { Box } from "./styled";
+import { useCurrentDate } from "./useCurrentDate";
 
 const formatDate = (date) =>
   date.toLocaleString(undefined, {
@@ -11,17 +11,7 @@ const formatDate = (date) =>
   });
 
 export const Clock = () => {
-  const [date, setDate] = useState(new Date());
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setDate(new Date());
-    }, 1000);
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
+  const date = useCurrentDate();
 
   return <Box>{formatDate(date)}</Box>;
 };
